6157676760413e053a66ec5dc51abea3c7d5398a
CaelanTaylor <92650991+CaelanTaylor@users.noreply.github.com>
2025-08-12 15:47:48 +1200

    Print

--- recv.py
+++ recv.py
@@ -33,6 +33,7 @@ def recv():
             windspd = message[0]
             atemp = message[1]
             wtemp = message[2]
+            print(f"Wind Speed: {windspd} m/s, Air Temp: {atemp} °C, Water Temp: {wtemp} °C")
 
 t1 = threading.Thread(target=recv)
 
