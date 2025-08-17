9a42fd7a4432b15ebf7455606710d72b543fa2a5
CaelanTaylor <92650991+CaelanTaylor@users.noreply.github.com>
2025-08-17 12:27:42 +1200

    360m

--- ai.py
+++ ai.py
@@ -32,7 +32,7 @@ else:
 
   
 response = ollama.chat(
-    model='smollm2:135m',  # or 'mistral', etc.
+    model='smollm2:360m',  # or 'mistral', etc.
     messages=[
         {'role': 'user', 'content': f"Here is the weather forecast:\n\n{data}\n\nCreate a simple forecast for the morning, noon, afternoon, evening, night and early morning for each of the days. Units in knots and celsius"}
     ]
