bdf2fa756a9d66574db9e00807f30181880f5151
CaelanTaylor <92650991+CaelanTaylor@users.noreply.github.com>
2025-09-26 11:19:36 +1200

    test

--- send.py
+++ send.py
@@ -27,7 +27,7 @@ def get_wind_speed():
 def get_wind_dir():
     """Get wind speed from the anemometer (channel 7)."""
     raw_value = read_channel(1)
-    wind_dir = ((raw_value - 199) / 824) * 360  # Convert to degrees
+    wind_dir = ((raw_value - 204) / 820) * 360  # Convert to degrees
     return wind_dir
 
 print(get_wind_speed())
