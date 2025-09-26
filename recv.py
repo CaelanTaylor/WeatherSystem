3f43339a58db790f6d1b071432dd982f4a1c07da
CaelanTaylor <92650991+CaelanTaylor@users.noreply.github.com>
2025-09-26 20:38:37 +1200

    test

--- recv.py
+++ recv.py
@@ -38,20 +38,18 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 def recv():
-    while True:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            HOST = ''  # Listen on all interfaces
-            PORT = 5000
-            s.bind((HOST, PORT))
-            s.listen(1)
-            conn, addr = s.accept()
-            with conn:
-                print('Connected by', addr)
-                data = conn.recv(1024)
-                if data:
-                    data_str = data.decode('utf-8')
-                    data_list = data_str.split(',')
-                    windspd, winddir, wtemp, atemp = map(float, data_list)
+   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        HOST = ''  # Listen on all interfaces
+        PORT = 5000
+        s.bind((HOST, PORT))
+        s.listen(1)
+        conn, addr = s.accept()
+        with conn:
+            data = conn.recv(1024)
+            if data:
+                data_str = data.decode('utf-8')
+                data_list = data_str.split(',')
+                windspd, winddir, wtemp, atemp = map(float, data_list)
 t1 = threading.Thread(target=recv)
 
 t1.start()
